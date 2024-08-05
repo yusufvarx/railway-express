80caa3103ff3a708faacab57ea242f04469e8267
yusufstawan <yusufsetiyawan45@gmail.com>
2024-08-05 23:14:30 +0700

    init project

---
+++ src/middlewares/acl.middleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { IReqUser } from "@/utils/interfaces";
+
+export default (roles: string[]) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const userRoles = (req as IReqUser).user.roles;
+
+    if (
+      !userRoles ||
+      !userRoles.some((userRole: string) => roles.includes(userRole))
+    ) {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+
+    next();
+  };
---
+++ src/routes.ts
@@ -0,0 +1,43 @@
+import express from "express";
+
+// middlewares
+import uploadMiddleware from "./middlewares/upload.middleware";
+import authMiddleware from "./middlewares/auth.middleware";
+
+// controllers
+import uploadController from "./controllers/upload.controller";
+import productsController from "./controllers/products.controller";
+import categoriesController from "./controllers/categories.controller";
+import authController from "./controllers/auth.controller";
+import aclMiddleware from "./middlewares/acl.middleware";
+
+const router = express.Router();
+
+// categories
+router.get("/categories", categoriesController.findAll);
+router.post("/categories", categoriesController.create);
+router.get("/categories/:id", categoriesController.findOne);
+router.put("/categories/:id", categoriesController.update);
+router.delete("/categories/:id", categoriesController.delete);
+
+// products
+router.get("/products", productsController.findAll);
+router.post("/products", productsController.create);
+router.get("/products/:id", productsController.findOne);
+router.put("/products/:id", productsController.update);
+router.delete("/products/:id", productsController.delete);
+
+// auth
+router.post("/auth/login", authController.login);
+router.post("/auth/register", authController.register);
+router.get(
+  "/auth/me",
+  [authMiddleware, aclMiddleware(["admin"])],
+  authController.me
+);
+router.put("/auth/profile", authMiddleware, authController.profile);
+
+router.post("/upload", uploadMiddleware.single, uploadController.single);
+router.post("/uploads", uploadMiddleware.multiple, uploadController.multiple);
+
+export default router;
